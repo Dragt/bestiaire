db0341225ab18fe737a705030971f9a544e6318c
Dragt <clerbois@yahoo.fr>
2019-12-18 03:12:08 +0100

    Create bestiaire.js

---
+++ js/bestiaire.js
@@ -0,0 +1,119 @@
+"use strict";
+
+
+/* ***** Utilitaire ***** */
+
+const gid = document.getElementById.bind(document);
+
+/* ***** initialisation ***** */
+
+document.addEventListener("DOMContentLoaded", initPage);
+
+function initPage() {
+	construireListes();
+	gid("selectMonstre").addEventListener("change", changerMonstre);	
+	gid("selectTemplate").addEventListener("change", changerTemplate);	
+	gid("selectAge").addEventListener("change", changerAge);
+	gid("buttonChercher").addEventListener("click", chercherMonstre);
+}
+
+function construireListes() {
+	construireListeMonstres();
+	construireListeTemplates();
+}
+
+function construireListeMonstres() {
+	gid("selectMonstre").innerHTML = monstres.map(m => 
+		`<option value="${m.nom}" data-famille="${m.famille}" data-niv="${m.niv}" data-vlc="${m.vlc}">${m.nom}</option>`)
+		.join(""); 
+	changerMonstre();
+}
+
+function construireListeTemplates() {
+	gid("selectTemplate").innerHTML = 
+		`<option value="" data-feminin="" data-niv="0" data-vlc="0">Aucun</option>` +
+		templatesMonstres.map(t => 
+		`<option value="${t.nom}" data-feminin="${t.famille}" data-niv="${t.niv}" data-vlc="${t.vlc}">${t.nom}</option>`)
+		.join("");
+	changerTemplate();
+}
+
+function mettreAjourListeAges() {
+	const familleDuMonstre = gid("selectMonstre").selectedOptions[0].dataset.famille;
+	gid("selectAge").innerHTML = ages[familleDuMonstre].map((a, i) => 
+		`<option value="${a}" data-niv="${i}" >${a}</option>`)
+		.join("");
+	changerAge();
+}
+
+
+/* ***** actions ***** */
+
+function changerMonstre() {
+	const monstreDataset = gid("selectMonstre").selectedOptions[0].dataset;
+	gid("familleAffichee").innerText = monstreDataset.famille;
+	gid("nivAffiche").innerText = monstreDataset.niv;
+	gid("vlcAffiche").innerText = (monstreDataset.vlc === 1 ? "oui" : "non");
+	mettreAjourListeAges();
+}
+
+function changerTemplate() {
+	const templateDataset = gid("selectTemplate").selectedOptions[0].dataset;
+	
+	gid("nivTemplate").innerText = (templateDataset.niv > -1 ? "+" : "") + templateDataset.niv;
+	gid("vlcTemplate").innerText = (templateDataset.vlc > 0 ? "oui" : "");
+	
+}
+
+function changerAge() {
+	gid("nivAge").innerText = gid("selectAge").selectedOptions[0].dataset.niv;
+}
+
+function chercherMonstre() {
+	const nom = gid("selectMonstre").value;
+	const template = gid("selectTemplate").value;
+	const age = gid("selectAge").value;
+	const id = gid("inputNumero").value;
+	
+	let criteres = {};
+	criteres.id = (id ? id : "4000001");
+	criteres.nom = nom + (template ? (" " + template) : "") + " [" + age + "]"; 
+	// todo : tester avec template devant si rien trouvé... ou avoir liste des templates à mettre devant
+	
+	let formData = new FormData();
+	formData.append("l", JSON.stringify([criteres]));
+
+	let request = new XMLHttpRequest();
+	request.open("POST", "https://mz.mh.raistlin.fr/mz/getCaracMonstre.php");
+	
+	
+	request.onload = traiterCdM;
+	//function () {console.log(request.responseText); alert(request.responseText);};
+		
+	request.send(formData);
+}
+
+function traiterCdM() {
+	try {
+		const cdm = JSON.parse(this.responseText);
+		console.log(cdm);
+		let carac = "";
+		for (let k in cdm[0]) {
+			if (typeof cdm[0][k] === 'object' && cdm[0][k] !== null) {
+				carac += "<p>" + k + " : <br>";
+				for (let kk in cdm[0][k]) {
+					carac += " " + kk + " : " + cdm[0][k][kk] + "<br>";
+				}
+				carac += "</p>";
+			}
+			else {
+				carac += `<p>${k} : ${cdm[0][k]}</p>`;
+			}
+		}
+		gid("affichageCaracteristiques").innerHTML = carac;
+	}
+	catch(e) {
+		console.log(e);
+		alert("Problème rencontré.");
+	}
+}
